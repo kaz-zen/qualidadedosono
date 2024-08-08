589d6bbb50d7287acb70b3e36ad88cba592fc6e6
kaz-zen <lucass.hernandeez@gmail.com>
2024-08-08 19:38:58 -0300

    new file:   __pycache__/correlacao.cpython-312.pyc 	new file:   __pycache__/graficos.cpython-312.pyc 	modified:   __pycache__/main.cpython-312.pyc 	modified:   __pycache__/modeloprevisao.cpython-312.pyc 	modified:   correlacao.py 	new file:   graficos.py 	modified:   main.py 	modified:   modeloprevisao.py

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder #scikit-learn
 from sklearn.preprocessing import StandardScaler
 import tratamentododataset as tratamento
+import correlacao as correlacao  
+import graficos as graficos
 
 df = tratamento.retornarDataframeTratado()
 
-
+correlacao.plotarGraficoCorrelacao(df)
+graficos.plotarGraficos(df)
 
 print(df.head())
